ddc37c99537a9dd457f281ae978e48b1eb47b5ec
Sebastian Perez <sebastian.perez@nuance.com>
2016-08-30 02:29:45 -0400

    fixes #5

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -110,40 +110,5 @@ pub fn reverse_string(s: &str) -> String {
 }
 
 
-/*
-pub fn fuse(dict: &HashMap<String, f32>) -> HashMap<String, f32> {
-    //! takes in a hash map and adds the probability of the minimum two
-    //! and returns a new hash map with the combined values
-    
-    let mut new_dict: HashMap<String, f32> = dict.clone();
-    
-    if new_dict.len() <= 1 {
-        return new_dict;
-    }
-    
-    // remove two letters
-    let first_letter = minimum(&new_dict);
-    let mut new_prob: f32;
-    {
-        let first_prob = new_dict.get(&first_letter).unwrap();
-        new_prob =  *first_prob;
-    }
-    new_dict.remove(&first_letter);
-    
-    let second_letter = minimum(&new_dict);
-    {
-        let second_prob = new_dict.get(&second_letter).unwrap();
-        new_prob = new_prob + *second_prob;
-    }
-    new_dict.remove(&second_letter);
-    let new_letter = first_letter + &second_letter;
-    
-    // create the new entry
-    new_dict.insert(new_letter, new_prob);
-    
-    new_dict
-
-}*/
-
 
 
